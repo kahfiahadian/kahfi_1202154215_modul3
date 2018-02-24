7a4b936ba6011fb62a6660a38db23381acf00088
kahfi <kahfi@github.com>
2018-02-24 23:52:23 +0700

    satu

---
+++ app/src/main/java/id/ac/telkomuniversity/student/kahfiahadian/kahfi_1202154215_modul3/Splashscreen.java
@@ -0,0 +1,22 @@
+package id.ac.telkomuniversity.student.kahfiahadian.kahfi_1202154215_modul3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 3500);
+    }
+}
